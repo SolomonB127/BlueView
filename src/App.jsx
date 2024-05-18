89c68732f34612e9878a35a825ba7d3182a635af
SolomonB127 <solomonakinsulire@gmail.com>
2024-05-18 14:02:17 +0100

    Changed how components are being rendered(using BrowserRouter & Routerprovider)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+// Importations of hooks
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// importations of components & pages
+import Layout from './assets/Components/Layout';
 import Header from './assets/Components/Header'
 import './App.css'
 
+// Routes definitions
+const router = createBrowserRouter([
+  {
+    path: '/', element: <Layout/>
+  }
+])
 function App() {
 
   return (
-    <>
-    <Header />
-    </>
+    <div className="App">
+      <RouterProvider router={router}/>
+    </div>
   )
 }
 
