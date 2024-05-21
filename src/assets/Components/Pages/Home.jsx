b747da1ecfe93318100ad6e93d7fe97851818fdc
SolomonB127 <solomonakinsulire@gmail.com>
2024-05-21 14:32:51 +0100

    Did the cover-image section of the Home page

--- src/assets/Components/Pages/Home.jsx
+++ src/assets/Components/Pages/Home.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
-
+import React from 'react';
+import './stylesheets/Home.css'
+import CoverImg from '/src/assets/Images/cover-img.png'
 const Home = () => {
     return (
-        <div>Home</div>
+    <main>
+        {/* Cover Image */}
+        <section className="cover-wrapper">
+            <div className="paddings inner-width flex-center" id='cover-img-container'>
+
+                <div className="flex-col-start cover-left">
+                    <section className='cover-title'>
+                        <section className="glow" />
+                        <h1>Discover <br /> More Suitable <br /> Property</h1>
+                    </section>
+
+                    <section className="flex-col-start cover-desc">
+                        <span>
+                            find a variety of properties that suits you very easily.
+                        </span>
+                        <span>
+                            forget all difficulties in finding a residence for you.
+                        </span>
+                    </section>
+
+                    <section className="search-bar">
+                        searchBar
+                    </section>
+                </div>
+
+                <div className="flex-center cover-right">
+                    <section className="img-container">
+                        <img src={CoverImg} alt="" />
+                    </section>
+                </div>
+            </div>
+        </section>
+    </main>
     )
 }
 
