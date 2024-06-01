7028376ea474382a91d7b839b78093b279537216
SolomonB127 <solomonakinsulire@gmail.com>
2024-06-01 16:17:20 +0100

    Created a utils folder(which the server.js file is inside) that acts as a mini-selfmade server

---
+++ src/assets/utils/server.js
@@ -0,0 +1,44 @@
+import Img1 from '/src/assets/Images/r1.png';
+import Img2 from '/src/assets/Images/r2.png';
+import Img3 from '/src/assets/Images/r3.png';
+import Img4 from '/src/assets/Images/r1.png';
+import Img5 from '/src/assets/Images/r2.png';
+import Img6 from '/src/assets/Images/r3.png';
+export default[
+    {
+    "name": "Aliva Priva Jardin",
+    "price": "47,043",
+    "detail": "Jakarta Garden City Street, Cakung. Pulo Gadung, Jakarta Timur, DKI Jakarta",
+    "image": Img1
+    },
+    {
+    "name": "Asatti Garden City",
+    "price": "66,353",
+    "detail": "Pahlawan Street XVII No.215, Cinangka, Sawangan, Depok, Jawa Barat",
+    "image": Img2
+    },
+    {
+    "name": "Citralan Puri Serang",
+    "price": "35,853",
+    "detail": "Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang, Banten",
+    "image": Img3
+    },
+    {
+    "name": "Aliva Priva Jardin",
+    "price": "47,043",
+    "detail": "Jakarta Garden City Street, Cakung. Pulo Gadung, Jakarta Timur, DKI Jakarta",
+    "image": Img4
+    },
+    {
+    "name": "Asatti Garden City",
+    "price": "66,353",
+    "detail": "Pahlawan Street XVII No.215, Cinangka, Sawangan, Depok, Jawa Barat",
+    "image": Img5
+    },
+    {
+    "name": "Citralan Puri Serang",
+    "price": "35,853",
+    "detail": "Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang, Banten",
+    "image": Img6
+    }
+]
