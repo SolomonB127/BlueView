35464741c23251fe1f375e587017f8b714383b18
SolomonB127 <solomonakinsulire@gmail.com>
2024-06-04 16:32:47 +0100

    Modified the accordion rendering in Home.jsx page

--- src/assets/Components/Pages/Home.jsx
+++ src/assets/Components/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './stylesheets/Home.css';
 
 // Images
@@ -167,21 +167,31 @@ const Home = () => {
                         allowMultipleExpanded={0}
                         preExpanded={[0]}
                         >
-                        {Adata.map((item, i) => (
-                            <AccordionItem className='accodionItem' key={i} uuid={i}>
-                                <AccordionItemHeading>
-                                    <AccordionItemButton>
-                                        <div className="flex-center icon">{item.icon}</div>
-                                        <span className="primaryText">
-                                            {item.heading}
-                                        </span>
-                                        <div className="flex-center icon">
-                                            <MdOutlineArrowDropDown size={20}/>
-                                        </div>
-                                    </AccordionItemButton>
-                                </AccordionItemHeading>
-                            </AccordionItem>
-                        ))}
+                        {Adata.map((item, i) => {
+                            const [className, setClassName] = useState("collapsed");
+                            return(
+                                <AccordionItem className={`accordionItem ${className}`} key={i} uuid={i}>
+                                    <AccordionItemHeading>
+                                        <AccordionItemButton className='flex-center accordionBtn'>
+                                            <AccordionItemState>
+                                                {({expanded}) => expanded ? setClassName("expanded") : setClassName("collapsed")}
+                                            </AccordionItemState>
+                                            <div className="flex-center icon">{item.icon}</div>
+                                            <span className="primaryText">
+                                                {item.heading}
+                                            </span>
+                                            <div className="flex-center icon">
+                                                <MdOutlineArrowDropDown size={20}/>
+                                            </div>
+                                        </AccordionItemButton>
+                                    </AccordionItemHeading>
+                                    <AccordionItemPanel>
+                                        <p className="secondaryText">{item.detail}</p>
+                                    </AccordionItemPanel>
+                                </AccordionItem>    
+                            )
+                        }
+                        )}
                     </Accordion>
                 </section>
             </div>
