ca05c76ead567c35e64a769fe498ea645c0b0cb9
SolomonB127 <solomonakinsulire@gmail.com>
2024-06-06 17:34:30 +0100

    Successfully rendered Footer unto page from Layout.jsx

--- src/assets/Components/Layout.jsx
+++ src/assets/Components/Layout.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './Header';
 import { Outlet } from 'react-router-dom'
+import Footer from './Footer';
 const Layout = () => {
     return (
         <>
-        <Header/>
-            <main>
-                <Outlet/> 
-            </main>
+            <Header/>
+                <main>
+                    <Outlet/> 
+                </main>
+            <Footer/>
         </>
     )
 }
