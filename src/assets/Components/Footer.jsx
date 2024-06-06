8be70fa8c14b8f93495c12e7bf9322c6594b1710
SolomonB127 <solomonakinsulire@gmail.com>
2024-06-06 17:32:55 +0100

    Imported and rendered site logo unto page. Added a few writings

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
-
+import React from 'react';
+import './stylesheets/Footer.css';
+import Logo from '/src/assets/Images/BlueView-logo.png'
 const Footer = () => {
     return (
-    <div>Footer</div>
+        <section className="f-wrapper">
+            <div className="paddings inner-width flex-center f-container">
+                {/* left side */}
+                <section className="flex-col-start f-left">
+                    <img src={Logo} alt="" width={120}/>
+                    <span className="secondaryText">
+                    Our vision is to make everywhere feel like home, for everyone.
+                    </span>
+                </section>
+            </div>
+        </section>
     )
 }
 
