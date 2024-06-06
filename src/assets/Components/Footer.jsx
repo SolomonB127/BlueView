4691050b7cdb4f2c8edc96a094859e97eb035151
SolomonB127 <solomonakinsulire@gmail.com>
2024-06-06 17:51:21 +0100

    Added final pieces of information to page

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -5,12 +5,24 @@ const Footer = () => {
     return (
         <section className="f-wrapper">
             <div className="paddings inner-width flex-center f-container">
-                {/* left side */}
                 <section className="flex-col-start f-left">
-                    <img src={Logo} alt="" width={120}/>
+                    <img src={Logo} alt="" width={90}/>
                     <span className="secondaryText">
                     Our vision is to make everywhere feel like home, for everyone.
                     </span>
+                </section>
+
+                <section className="flex-col-start f-right">
+                    <span className='primaryText'>Information</span>
+                    <span className='secondaryText'>Km 32 Lekki - Epe Expressway,
+                        Awoyaya, Lagos, Nigeria.
+                    </span>
+                    <div className="flex-center f-menu">
+                        <span>Property</span>
+                        <span>Services</span>
+                        <span>Product</span>
+                        <span>About Us</span>
+                    </div>
                 </section>
             </div>
         </section>
